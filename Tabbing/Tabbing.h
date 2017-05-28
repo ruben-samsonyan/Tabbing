807e1f2fd401c1a6c9fb4eb28591479af13ce288
Ruben Samsonyan <ruben.samsonyan@gmail.com>
2017-05-28 16:41:04 +0400

    Initial Commit

---
+++ Tabbing/Tabbing.h
@@ -0,0 +1,19 @@
+//
+//  Tabbing.h
+//  Tabbing
+//
+//  Created by Ruben Samsonyan on 5/28/17.
+//  Copyright © 2017 Ruben Samsonyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tabbing.
+FOUNDATION_EXPORT double TabbingVersionNumber;
+
+//! Project version string for Tabbing.
+FOUNDATION_EXPORT const unsigned char TabbingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tabbing/PublicHeader.h>
+
+
